3e9327ff2434733a99c7e0a4839f978d1775debb
gabrielfaria87 <gabrieldealmeidafaria@outlook.com>
2025-05-13 15:09:44 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <h1>Gabriel Faria</h1>
+      <p>Sargento PM programador FULLSTACK</p>
+    </header>
+
+    <main>
+<h2>Sobre mim</h2>
+<p>Lorem ipsum dolor sit, amet <strong>consectetur</strong> adipisicing elit. Laborum hic nulla 
+  vel ullam neque, consequuntur blanditiis illum consectetur consequatur corporis 
+  pariatur maiores dolores aliquam quis labore rerum accusantium cum nobis.</p>
+
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum repellat accusamus eos cupiditate
+     qui mollitia autem! Harum, impedit. Odit asperiores enim dolores, facilis corporis saepe. Molestias 
+     repudiandae sed itaque quas!</p>
+
+     <h3>Habilidades principais</h3>
+     <ul>
+      <li>Montagem e configuração de PCs</li>
+      <li>Redes e conectividades</li>
+      <li>Sistemas operacionais (windows, linux, MS-DOS)</li>
+      <li>Didática técnica para cursos e certificações</li>
+
+     </ul>
+    </main>
+
+    <footer>
+     &copy; Gabriel Faria - Todos os direitos reservados
+    </footer>
+
+    </>
+  )
+}
+
+export default App
